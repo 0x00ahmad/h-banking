16f0ea5001d2dc869086b237992ef0ed1f9f371f
0x00ahmad <mahmadrehan00@gmail.com>
2024-07-09 01:43:53 +0300

    added better err handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ export const dynamic = "force-dynamic";
 
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        <Toaster />
         {children}
       </body>
     </html>
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
-import { Input } from './ui/input'
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
 
-import { Control, FieldPath } from 'react-hook-form'
-import { z } from 'zod'
-import { authFormSchema } from '@/lib/utils'
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
 
-const formSchema = authFormSchema('sign-up')
+const formSchema = authFormSchema("sign-up");
 
 interface CustomInput {
-  control: Control<z.infer<typeof formSchema>>,
-  name: FieldPath<z.infer<typeof formSchema>>,
-  label: string,
-  placeholder: string
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+  inputType?: string;
+  maxLength?: number;
 }
 
-const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+  inputType,
+}: CustomInput) => {
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
-        <div className="form-item">
-          <FormLabel className="form-label">
-            {label}
-          </FormLabel>
+        <div className="form-item w-full">
+          <FormLabel className="form-label">{label}</FormLabel>
           <div className="flex w-full flex-col">
-            <FormControl>
-              <Input 
+            <FormControl className="w-full">
+              <Input
                 placeholder={placeholder}
-                className="input-class"
-                type={name === 'password' ? 'password' : 'text'}
+                className="input-class w-full"
+                type={
+                  name === "password"
+                    ? "password"
+                    : inputType
+                      ? inputType
+                      : "text"
+                }
                 {...field}
               />
             </FormControl>
@@ -39,7 +51,7 @@ const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
         </div>
       )}
     />
-  )
-}
+  );
+};
 
-export default CustomInput
+export default CustomInput;
